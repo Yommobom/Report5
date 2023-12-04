3b83ba23df072e4f3228d049cf4f652837c79d64
Nishime Kenta <e235703@ie.u-ryukyu.ac.jp>
2023-12-04 18:23:28 +0900

    3rd

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生したぞい");
-            System.out.println("エラー：" + e.getMessage());
-        }
+       String str = "壱百満";
+       int value = Integer.parseInt(str);
+       System.out.println(value);
 
     }
     
